fb1a06bd0f70850e3a8a9a94d9ee760ae2ce23e8
Kemo-Alamy <kareemammar2025@gmail.com>
2026-04-27 07:14:05 +0300

    Update Telegram and WhatsApp configuration settings

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 export default {
   // ===== إعدادات تليجرام =====
-  TELEGRAM_BOT_TOKEN: "YOUR_BOT_TOKEN_HERE",   // ضع التوكن الذي من @BotFather
-  ADMIN_IDS: [123456789],                      // ضع معرف التليجرام الخاص بك (أدمن)
-  REQUIRED_CHANNEL: null,                     // ضع معرف القناة إن أردت اشتراكًا إجباريًا (مثل "@channel")
+  TELEGRAM_BOT_TOKEN: "8112888424:AAF3j-EoZT3HVNr-t8aBslMHF2h7OypbfDM",   // ضع التوكن الذي من @BotFather
+  ADMIN_IDS: [6520549428],                      // ضع معرف التليجرام الخاص بك (أدمن)
+  REQUIRED_CHANNEL: "@Crashkemobot" ,                     // ضع معرف القناة إن أردت اشتراكًا إجباريًا (مثل "@channel")
 
   // ===== إعدادات واتساب =====
   FIXED_PAIRING_CODE: "KEMO KING",             // الكود الثابت للربط (يعمل في سيناريو 2099)
-  AUTHORIZED_NUMBERS: ["+201234567890"],       // أرقام واتساب المسموح لها بإرسال أوامر (ضع رقمك)
+  AUTHORIZED_NUMBERS: ["+201115168017"],       // أرقام واتساب المسموح لها بإرسال أوامر (ضع رقمك)
 
   // ===== إعدادات الهجوم =====
   REPORTS_PER_ACCOUNT: 500,                    // 500 بلاغ لكل حساب واتساب مرتبط
